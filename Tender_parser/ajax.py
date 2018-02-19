6668c8eac0a5566aae0ae5e609ccff3fa04f0452
Rinat Nuryev <rnuryev@gmail.com>
2018-02-19 18:08:34 +0500

    В AJAX сделал запросы через requests

--- Tender_parser/ajax.py
+++ Tender_parser/ajax.py
@@ -1,32 +1,60 @@
 from requests import Session
+import requests
 
-session = Session()
-session.head('https://etp.rosseti.ru/#com/procedure/index')
+url = 'https://rt-ci.ru/modules/filterTable.php'
+data = {
+    'company': None,
+    'pt': 0,
+    'nmcs': None,
+    'nmce': None,
+    'valute': 0,
+    'ds': None,
+    'de': None,
+    'nd': None,
+    's_search': 'поставка'
+}
 
-resp = session.post(
-    url ='https://etp.rosseti.ru/index.php?rpctype=direct&module=default',
-    headers={
-        'Host': 'etp.rosseti.ru',
-        'Connection': 'keep-alive',
-        'Content-Length': '150',
-        'Origin': 'https://etp.rosseti.ru',
-        'X-Requested-With': 'XMLHttpRequest',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36',
-        'Content-Type': 'application/json',
-        'Accept': '*/*',
-        'Referer': 'https://etp.rosseti.ru/',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-        'Cookie': '_GUEST_ID=9584475; _ym_uid=1518008571464525230; _LAST_VISIT=07.02.2018+16%3A04%3A13; etpsid2=trdgf835e3mtepmpavvo33t2j3',
-    },
-    data={
-        "action": "Procedure",
-        "method": "list",
-        "data": [{"start": 0, "limit": 1000, "sort": "id", "dir":"DESC"}],
-        "type": "rpc",
-        "tid": 5,
-        "token": "TtQD8za7RvzheZ09tRrfag"
-    }
-)
+# def get_html(url, data):
+r = requests.post(url, data=data)
+    # return r.text
 
-print(resp.text)
+
+# session = Session()
+# session.head('https://rt-ci.ru/modules/filterTable.php')
+#
+# resp = session.post(
+#     url ='https://rt-ci.ru/modules/filterTable.php',
+#     # headers={
+#     #     'Host': 'etp.rosseti.ru',
+#     #     'Connection': 'keep-alive',
+#     #     'Content-Length': '150',
+#     #     'Origin': 'https://etp.rosseti.ru',
+#     #     'X-Requested-With': 'XMLHttpRequest',
+#     #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36',
+#     #     'Content-Type': 'application/json',
+#     #     'Accept': '*/*',
+#     #     'Referer': 'https://etp.rosseti.ru/',
+#     #     'Accept-Encoding': 'gzip, deflate, br',
+#     #     'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+#     #     'Cookie': '_GUEST_ID=9584475; _ym_uid=1518008571464525230; _LAST_VISIT=07.02.2018+16%3A04%3A13; etpsid2=trdgf835e3mtepmpavvo33t2j3',
+#     # },
+#     data={
+#         # "action": "Procedure",
+#         # "method": "list",
+#         # "data": [{"start": 0, "limit": 1000, "sort": "id", "dir":"DESC"}],
+#         # "type": "rpc",
+#         # "tid": 5,
+#         # "token": "TtQD8za7RvzheZ09tRrfag"
+#         'company': None,
+#         'pt': 0,
+#         'nmcs': None,
+#         'nmce': None,
+#         'valute': 0,
+#         'ds': None,
+#         'de': None,
+#         'nd': None,
+#         's_search': 'поставка'
+#     }
+# )
+
+print(r.text)
